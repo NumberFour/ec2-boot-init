af1a24ccc66e781f04cc0000518419e22e4d989d
Romain PELISSE <romain.pelisse@numberfour.eu>
2011-05-17 17:04:26 +0200

    adds an --update option to ec2-boot-init to only refresh variables

--- ec2-boot-init.rb
+++ ec2-boot-init.rb
@@ -1,14 +1,40 @@
 #!/usr/bin/ruby
 
 require 'ec2boot'
-localmode=false
+require 'optparse'
 
 def log(msg)
     puts "#{Time.now}> #{msg}"
     system("logger", "#{msg}")
 end
 
-puts "Network mode enabled"
+localmode=false
+bootmode=true
+debug=false
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: TODO --update"
+
+  opts.on("-u", "--update", "only updates ec2 variables values") do |v|
+    bootmode=false
+  end
+
+  opts.on("-l", "--local-mode", "Runs locally") do |v|
+    localmode=true
+  end
+
+  opts.on("-d", "--debug", "Run in debug mode") do |v|
+    debug=true
+  end
+
+end.parse!
+
+if debug
+   log "bootmode:#{bootmode}"
+   log "localmode:#{localmode}"
+   log "debug:#{debug}"
+end
+
 config = EC2Boot::Config.new
 
 ud = EC2Boot::UserData.new(config)
@@ -17,8 +43,12 @@ md = EC2Boot::MetaData.new(config)
 EC2Boot::Util.write_facts(ud, md, config)
 log "Facts written."
 
-config.actions.run_actions(ud, md, config)
-log "Action run."
+if bootmode
+    log "Boot mode enabled" if debug
+    log "boot instance"
+    config.actions.run_actions(ud, md, config)
+    log "Action run."
 
-EC2Boot::Util.update_motd(ud, md, config)  if ud.fetched? && md.fetched?
-log "Motd updated."
+    EC2Boot::Util.update_motd(ud, md, config)  if ud.fetched? && md.fetched?
+    log "Motd updated."
+end
