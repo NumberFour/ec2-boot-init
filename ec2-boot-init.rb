2d8832837f345b3a89d967f006580a102a6fff14
Romain PELISSE <romain.pelisse@numberfour.eu>
2011-05-19 16:32:52 +0200

    makes update mode default and boot mode triggered by the --boot option

--- ec2-boot-init.rb
+++ ec2-boot-init.rb
@@ -8,16 +8,16 @@ def log(msg)
     system("logger", "#{msg}")
 end
 
-bootmode=true
+bootmode=false
 debug=false
 
 OptionParser.new do |opts|
     opts.banner = "ec2-boot-init: A script to provision EC2 boot instances."
 
-    opts.on("-u", "--update", "only updates ec2 variables values") do |v|
+    opts.on("-b", "--boot", "will runs actions along with updating facts.") do |v|
         bootmode=false
     end
-    
+
     opts.on("-d", "--debug", "Run in debug mode") do |v|
         debug=true
     end
@@ -33,14 +33,16 @@ config = EC2Boot::Config.new
 ud = EC2Boot::UserData.new(config)
 md = EC2Boot::MetaData.new(config)
 
+log "Updating facts..."
 EC2Boot::Util.write_facts(ud, md, config)
-log "Facts written."
+log "...facts written."
 
 if bootmode
     log "Boot mode enabled." if debug
+    log "Run actions..."
     config.actions.run_actions(ud, md, config)
-    log "Action run."
+    log "...done."
 end
-
+# update motd in case some value it uses has been updated.
 EC2Boot::Util.update_motd(ud, md, config)  if ud.fetched? && md.fetched?
 log "Motd updated."
